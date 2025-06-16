26a3bb4c34d77d20002618995ac50493f58c30cb
Vedika-2006-coder <pvedika28@gmail.com>
2025-06-16 06:50:09 +0000

    datatypes

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,12 @@
+const account_id=2356
+let account_email="vedikapatil@gmail.com"
+var account_password="236799"
+account_city="Sangli"
+console.log(account_id)
+let account_State
+/* prefer not to use var 
+because var is functional-scope */
+account_email="trupti@gmail.com"
+account_password="698008"
+account_city="Pune"
+console.table([account_id,account_email,account_password,account_city,account_State])
---
+++ 01_Basics/Datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all js code as newer version
+// alter(3+3)// it gives error,because we are using nodejs,not browser
+//-- primitive datatypes-
+// Number-> 2 to power 53
+// BigInt->
+// String-> " "
+// Boolean->true/false
+// undefined-> 
+// null-> standalone value
+// Symbol->Unique
+
+//-- nonprimitive datatypes
+// Object-> key-value pairs
+// Array->ordered list of values
+// Function
+// Date ,RegExp-> Special built-in object types
