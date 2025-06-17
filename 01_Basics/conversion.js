da47164fb3c4726321dbe383c4fcc8e9d9bba312
Vedika-2006-coder <pvedika28@gmail.com>
2025-06-17 08:44:18 +0000

    conversion

---
+++ 01_Basics/conversion.js
@@ -0,0 +1,24 @@
+let score=33
+console.log(typeof(score))
+let a="60"
+let s=Number(a)
+console.log(typeof(s))
+let b=undefined
+console.log(typeof(b))
+let c=null
+console.log(typeof(c))
+let g="33abc"
+let n=Number(g)
+console.log(n) //NaN
+
+console.log(1+"2")
+console.log("1"+2)
+console.log("1"+"2")
+console.log(1+1+"2")
+console.log("1"+1+"2")
+console.log("1"+1+2)
+
+a=3
+b=3
+console.log(++a)
+console.log(b++)
