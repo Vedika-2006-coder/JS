5b0a01da44e413718cc51064789fa48a7c0c6ef0
Vedika-2006-coder <pvedika28@gmail.com>
2025-06-21 14:31:47 +0000

    two

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+// while
+let index=1
+while(index<=5)
+{
+    console.log("Hii,I am Tony")
+    index++
+}
+let arr=["tony","sam","john","ram"]
+let n=arr.length
+let i=0
+while (i<n) {
+    console.log(arr[i])
+    i++
+}
+
+// do while loop
+let num=11
+do
+{
+    console.log(`value of num is ${num}`)
+    num++
+}while(num<10)
