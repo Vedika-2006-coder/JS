6371e367e8068e282ebe666e023fa7175a133722
Vedika-2006-coder <pvedika28@gmail.com>
2025-06-21 14:30:51 +0000

    one1

---
+++ 04_Control_flow/one.js
@@ -0,0 +1,32 @@
+// if
+const userloggedin=true;
+if(userloggedin)
+{
+
+}
+// <,>,<=,>=,==,!=
+const temp=41
+if(temp>40)
+{
+    console.log("temp is greater than 40")
+}
+else
+{
+    console.log("temp is not greater than 40")
+}
+
+const score=200
+if(score>100)
+{
+    const power="fly"
+    console.log(`user power:${power}`)
+}
+const v=500
+if(v>200) console.log("score is grater than 200")
+
+const a=20
+const username="Tony"
+if(username=="Tony" && a>10)
+{
+    console.log("yes")
+}
